41c9b4542670041213f5044b80628778fd3d1111
손소독 <vot768@naver.com>
2023-07-14 14:40:20 +0900

    feat: players api 구현

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import goldpassRoutes from "./goldpass.routes";
 import healthcheckRoutes from "./healthcheck.routes";
 import { Router } from "express";
+import playersRoutes from "./players.routes";
 
 const router = Router();
 
 router.use(healthcheckRoutes());
 router.use(goldpassRoutes());
+router.use(playersRoutes());
 
 export default router;
---
+++ src/routes/players.routes.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import constants from "../utils/constants";
+import { Router, response } from "express";
+import asyncify from "express-asyncify";
+
+export default function routeGenerate() {
+    const router = asyncify(Router());
+
+    router.get("/players/:id", async (req, res) => {
+        const playerId = req.params.id;
+        axios({
+            method: 'GET',
+            url: constants.API_SERVER + `players/${encodeURIComponent(playerId)}`,
+            headers: {
+                Authorization: `Bearer ${constants.API_TOKEN}`
+            }
+        }).then(response => {
+            res.send(response.data);
+        }).catch(error => {
+            console.log(error);
+            res.status(500);
+        });
+    });
+
+    return router;
+}

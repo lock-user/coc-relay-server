e05b578b5116537970faf05a481d3ebd805f8796
손소독 <vot768@naver.com>
2023-07-17 11:16:36 +0900

    feat: locations api 구현

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,16 @@
 import clansRoutes from "./clans.routes";
 import goldpassRoutes from "./goldpass.routes";
 import healthcheckRoutes from "./healthcheck.routes";
-import { Router } from "express";
+import locationsRoutes from "./locations.routes";
 import playersRoutes from "./players.routes";
+import { Router } from "express";
 
 const router = Router();
 
 router.use(clansRoutes());
 router.use(healthcheckRoutes());
 router.use(goldpassRoutes());
+router.use(locationsRoutes());
 router.use(playersRoutes());
 
 export default router;

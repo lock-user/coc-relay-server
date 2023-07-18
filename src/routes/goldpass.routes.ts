4f06cfd4fbedcb9ef78fb4164866834a70337030
locker <dev.private11000100@proton.me>
2023-07-18 22:39:17 +0900

    feat: goldpass api 수정 - goldpass api 요청 경로 변경 - 에러 처리

--- src/routes/goldpass.routes.ts
+++ src/routes/goldpass.routes.ts
@@ -6,17 +6,20 @@ import asyncify from "express-asyncify";
 export default function routeGenerate() {
     const router = asyncify(Router());
 
-    router.get("/goldpass", async (req, res) => {
+    router.get("/goldpass/seasons/current", async (req, res) => {
         const requestHeader = {
             "Authorization": `Bearer ${constants.API_TOKEN}`
         };
 
         axios.get(`${constants.API_SERVER}goldpass/seasons/current`, { headers: requestHeader })
             .then(response => {
-                res.send(response.data);
+                res.status(200).send(response.data);
             }).catch(error => {
-                console.error(error);
-                res.status(500);
+                console.error(error);if(error.response) {
+                    res.status(error.response.status).send(error.response.data);
+                } else {
+                    res.status(500).send({ message: "Internal Server Error" });
+                }
             });
     });
 

07e983b1f35529c02689a22386e5099c8ce022a0
손소독 <vot768@naver.com>
2023-07-20 17:32:06 +0900

    refactor: axios로 goldpass api에 요청하는 로직 수정

--- src/routes/goldpass.routes.ts
+++ src/routes/goldpass.routes.ts
@@ -7,21 +7,23 @@ export default function routeGenerate() {
     const router = asyncify(Router());
 
     router.get("/goldpass/seasons/current", async (req, res) => {
-        const requestHeader = {
-            "Authorization": `Bearer ${constants.API_TOKEN}`
-        };
+        axios({
+            method: 'GET',
+            url: constants.API_SERVER + "goldpass/seasons/current",
+            headers: {
+                Authorization: `Bearer ${constants.API_TOKEN}`
+            }
+        }).then(response => {
+            res.status(200).send(response.data);
+        }).catch(error => {
+            console.error(error);
+            if(error.response) {
+                res.status(error.response.status).send(error.response.data);
+            } else {
+                res.status(500).send({ message: "Internal Server Error" });
+            }
+        })
 
-        axios.get(`${constants.API_SERVER}goldpass/seasons/current`, { headers: requestHeader })
-            .then(response => {
-                res.status(200).send(response.data);
-            }).catch(error => {
-                console.error(error);
-                if(error.response) {
-                    res.status(error.response.status).send(error.response.data);
-                } else {
-                    res.status(500).send({ message: "Internal Server Error" });
-                }
-            });
     });
 
     return router;

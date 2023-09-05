6ff3c408ab229b88ded7c6382b7ec95d770e05d8
손소독 <vot768@naver.com>
2023-09-05 10:30:34 +0900

    feat: nextjs 프로젝트로 재생성

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import { Express } from "express";
-import http from "http";
-
-export const getServer = (app?: Express) => {
-    let server = http.createServer(app);
-
-    return server;
-};
--- src/utils/constants.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    PORT: Number(process.env.PORT || 3333),
-    PROJECT_NAME: process.env.PROJECT_NAME || "coc-relay",
-    API_SERVER: process.env.SERVER,
-    API_TOKEN: process.env.TOKEN
-}

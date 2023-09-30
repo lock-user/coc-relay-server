458fc366ab0e1fea32213e233b7b843d38f6fd99
locker <dev.private11000100@proton.me>
2023-09-30 14:23:27 +0900

    feat: players api 구현

--- src/pages/api/players/[playerTag]/verifytoken.ts
+++ src/pages/api/players/[playerTag]/verifytoken.ts
@@ -1,1 +1,30 @@
-// TODO players/$playerTag/verifytoken api 구현 필요
+import {NextApiRequest, NextApiResponse} from "next";
+import axios from "axios";
+import constants from "@/utils/constants";
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method === "POST") {
+        const {playerTag} = req.query;
+        const {token} = req.body;
+
+        axios({
+            method: 'POST',
+            url: constants.API_SERVER + `players/${encodeURIComponent(playerTag as string)}/verifytoken`,
+            headers: {
+                Authorization: `Bearer ${constants.API_TOKEN}`
+            },
+            data: {
+                "token": token
+            }
+        }).then(response => {
+            res.status(200).send(response.data);
+        }).catch(error => {
+            res.status(error.response.status).send(error.response.data);
+        });
+    } else {
+        res.status(405).send({message: "Please request using the post method."})
+    }
+}
